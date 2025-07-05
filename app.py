7682509268bdf6c6085dd95e55a945f66c19d87f
abhinavad16 <abhinavdonadkar800@gmail.com>
2025-07-05 10:28:35 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
+import nltk
+nltk.download('punkt')
 import pickle
 import string
 from nltk.corpus import stopwords
-import nltk
 from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
